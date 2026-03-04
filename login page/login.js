b76d9e06745b45af3c8371195d6b84daca85d710
vincenttrinh9098 <vincenttrinh9098@gmail.com>
2026-03-03 17:14:28 -0800

    fixed URL

--- login page/login.js
+++ login page/login.js
@@ -26,7 +26,7 @@ googleBtn.addEventListener('click', (e) => {
             const user = result.user;
             console.log("Logged in:", user.displayName);
             
-            window.location.href = "/Main-Menu/Dashboard/dashboard.html"; 
+            window.location.href = "Main-Menu/Dashboard/dashboard.html"; 
         })
         .catch((error) => {
             console.error("Auth Error:", error.code);
